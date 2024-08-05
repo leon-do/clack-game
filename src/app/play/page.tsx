fd3cb2d2fa86c5b7aa5ac677e41466ee9789ee01
leon-do <leondo123@gmail.com>
2024-08-05 15:29:28 -0400

    frame

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,4 +1,16 @@
 "use client";
+import Nav from "../../components/Nav";
+import Button from "../../components/Button";
+
 export default function Play() {
-    return <div>play</div>;
+
+
+  return (
+    <div className="border border-red-400 container mx-auto h-screen">
+      <Nav />
+      <div className="border border-emerald-400 flex h-full justify-center items-center">
+        <Button />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,9 @@
+export default function Nav() {
+  return (
+    <div className="flex justify-center">
+      <div className="m-5">apple</div>
+      <div className="m-5">apple</div>
+      <div className="m-5">apple</div>
+    </div>
+  );
+}
